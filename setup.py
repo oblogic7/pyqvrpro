e7f06dd84cdb7137510e30917cfe96306ef1824b
Matt Snyder <matt@obscurelogic.com>
2019-02-15 22:26:37 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/oblogic7/pyqvrpro'
 EMAIL = 'matt@obscurelogic.com'
 AUTHOR = 'Matt Snyder'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = 0.42
+VERSION = 0.43
 
 # What packages are required for this module to be executed?
 REQUIRED = [
